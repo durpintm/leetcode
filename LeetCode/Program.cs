be5bc2cb8509894918ff6778949f881cb9607321
durpintm <durpinthapa@gmail.com>
2024-09-19 17:09:47 -0400

    Question 88

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,15 @@
-﻿public class Program
+﻿using LeetCode;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         //int[] result = TwoSum([2, 7, 11, 15], 9);
         //int[] result = SortedSquares([-4, -1, 0, 3, 10]);
-        int[] result = SortedSquares([1]);
+        //int[] result = SortedSquares([1]);
+        Question_88 question_88 = new Question_88();
+        //question_88.Merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+        question_88.Merge([0], 0, [1], 1);
         Console.WriteLine();
         Console.ReadKey();
     }
---
+++ LeetCode/Question_88.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Question_88
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] combinedArray = new int[m + n];
+            int index = m + n - 1;
+            int i, j;
+            for (i = m - 1, j = n - 1; i >= 0 && j >= 0;)
+            {
+                if (nums1[i] >= nums2[j])
+                {
+                    nums1[index] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[index] = nums2[j];
+                    j--;
+                }
+                index--;
+            }
+
+            while (i >= 0)
+            {
+                nums1[index] = nums1[i];
+                i--;
+                index--;
+            }
+
+            while (j >= 0)
+            {
+                combinedArray[index] = nums2[j];
+                j--;
+                index--;
+            }
+        }
+    }
+}
