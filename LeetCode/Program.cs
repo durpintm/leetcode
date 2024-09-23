9d8b93d27e0261b2f8ceff9e24b4505994ace0a1
durpintm <durpinthapa@gmail.com>
2024-09-22 20:22:01 -0400

    Spiral Order Problem-Leetcode 54

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,7 +12,10 @@ public class Program
         //Question_88.Merge2([0], 0, [1], 1);
 
         //GG_1.Segregate0and1([0, 0, 1, 1, 0]);
-        string s = Question_5.LongestPalindrome("sdfgrrgft");
+
+        //string s = Question_5.LongestPalindrome("sdfgrrgft");
+
+        var result = Question_54.SpiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]);
 
         Console.WriteLine();
         Console.ReadKey();
---
+++ LeetCode/Question_54.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal static class Question_54
+    {
+        public static IList<int> SpiralOrder(int[][] matrix)
+        {
+            int rows = matrix.Length;
+            int columns = matrix[0].Length;
+            int length = rows * columns;
+            int count = 0;
+            IList<int> list = new List<int>();
+            int left = 0, right = columns - 1, up = 0, down = rows - 1;
+
+            while (count < length)
+            {
+
+                // traverse from left to right
+                for (int col = left; col <= right; col++)
+                {
+                    list.Add(matrix[up][col]);
+                    count++;
+                }
+
+                // traverse from up to down
+                for (int row = up + 1; row <= down; row++)
+                {
+                    list.Add(matrix[row][right]);
+                    count++;
+                }
+
+                if (up != down)
+                {
+                    //traverse from right to left
+                    for (int col = right - 1; col >= left; col--)
+                    {
+                        list.Add(matrix[down][col]);
+                        count++;
+                    }
+                }
+
+
+                if (left != right)
+                {
+                    // travers from down to up
+                    for (int row = down - 1; row > up; row--)
+                    {
+                        list.Add(matrix[row][left]);
+                        count++;
+                    }
+                }
+
+                left++;
+                right--;
+                up++;
+                down--;
+            }
+
+            return list;
+        }
+    }
+}
