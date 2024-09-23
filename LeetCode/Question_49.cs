553944fd4f35e97522c33d6aa860dcd1fcff0b5f
durpintm <durpinthapa@gmail.com>
2024-09-23 18:23:59 -0400

    Group Anagrams

---
+++ LeetCode/Question_49.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Question_49
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+            foreach (string str in strs)
+            {
+                int[] freq = new int[26];
+
+                foreach (char ch in str.ToCharArray())
+                {
+                    freq[ch - 'a']++;
+                }
+
+                string freqPattern = "";
+
+                foreach (int charFreq in freq)
+                {
+                    freqPattern += (charFreq + "#");
+                }
+
+                if (!dict.ContainsKey(freqPattern))
+                {
+                    dict[freqPattern] = new List<string>();
+                }
+
+                dict[freqPattern].Add(str);
+            }
+
+            //    foreach (var kvp in dict)
+            //     {
+            //         Console.WriteLine($"{kvp.Key}:");
+
+            //         foreach (var item in kvp.Value)
+            //         {
+            //             Console.WriteLine($" - {item}");
+            //         }
+            //     }
+            return dict.Values.ToList<IList<string>>();
+        }
+    }
+}
